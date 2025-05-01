2d2f195409a75c8ba40437b72b1541ab6d8acddb
Pedro Fernandes <pedro.santos@gruporbs.com.br>
2025-05-01 17:20:06 -0300

    ✨ feat: json converter

---
+++ src/main/java/streaming/backend/utils/JsonUtil.java
@@ -0,0 +1,13 @@
+package streaming.backend.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class JsonUtil {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static String toJson(Object object) throws JsonProcessingException {
+        return mapper.writeValueAsString(object);
+    }
+}
